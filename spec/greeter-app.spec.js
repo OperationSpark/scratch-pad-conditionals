4dd16cb9814c33f2eab45f172608ff42ec49fd41
Ben Spector <ben@operationspark.org>
2019-03-25 20:55:39 +0000

    consolidated testing

--- spec/greeter-app.spec.js
+++ spec/greeter-app.spec.js
@@ -7,15 +7,16 @@ describe("#greeter-app", function () {
         console.log.restore();
     });
 
-    it('should print "Good Morning!" if hour is between 0 and 11', function () {
+    it('should print the correct message depending on the time of day', function () {
         greeter(0);
         expect(console.log.firstCall.calledWith('Good Morning!')).to.be.true;
         greeter(11);
         expect(console.log.secondCall.calledWith('Good Morning!')).to.be.true;
         greeter(12);
         expect(console.log.thirdCall.calledWith('Good Morning!')).to.be.false;
-    });
-    it('should print "Good Afternoon!" if hour is between 12 and 16', function () {
+    
+        console.log.reset();
+        
         greeter(12);
         expect(console.log.firstCall.calledWith('Good Afternoon!')).to.be.true;
         greeter(16);
@@ -23,16 +24,17 @@ describe("#greeter-app", function () {
         greeter(17);
         expect(console.log.thirdCall.calledWith('Good Afternoon!')).to.be.false;
         
-    });
-    it('should print "Good Evening!" if hour is between 17 and 21', function () {
+        console.log.reset();
+        
         greeter(17);
         expect(console.log.firstCall.calledWith('Good Evening!')).to.be.true;
         greeter(21);
         expect(console.log.secondCall.calledWith('Good Evening!')).to.be.true;
         greeter(22);
         expect(console.log.thirdCall.calledWith('Good Evening!')).to.be.false;
-    });
-    it('should print "Good Night!" if hour is between 22 and 23', function () {
+   
+        console.log.reset();
+        
         greeter(22);
         expect(console.log.firstCall.calledWith('Good Night!')).to.be.true;
         greeter(23);
